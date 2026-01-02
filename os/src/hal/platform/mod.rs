e601ad2c190dbb50851fe81b6643a9a2bf0c9c1b
pillow <waysorrypub@gmail.com>
2026-01-02 16:35:22 +0800

    为platform/riscv/mod和platform/mod添加详细注释

--- os/src/hal/platform/mod.rs
+++ os/src/hal/platform/mod.rs
@@ -1,4 +1,18 @@
-#[cfg(feature = "board_rvqemu")]
+//! PlatForm 统一管理模块
+//! # Overview
+//! 本模块根据编译特性选择实现底层运行平台（rvqemu laqemu 或 2k1000）
+//!
+//! # Design
+//! - 使用 `#[cfg(feature = "...")]` 根据运行平台选择模块
+//!
+//! # Assumptions
+//! - 编译时必须指定运行凭他（`rvqemu` `laqemu` 或 `2k1000`）
+//!
+//! # Safety
+//! - 被使用的模块设计底层 MMIO，时钟等关键数据，必须保证数据真实可靠
+
+
+#[cfg(feature = "riscv")]
 pub mod riscv;
 
 #[cfg(feature = "board_rvqemu")]
--- os/src/hal/platform/riscv/mod.rs
+++ os/src/hal/platform/riscv/mod.rs
@@ -1,1 +1,5 @@
+//! 内核运行平台管理模块（RISC-V）
+//! #Overview
+//! 本模块定义了在RISC-V架构下内核可以运行的平台
+
 pub mod qemu;
