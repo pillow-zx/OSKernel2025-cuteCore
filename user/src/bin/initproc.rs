0975f23f19f415421b333eedba43b3037fd78917
“zy” <3328039835@qq.com>
2026-01-11 21:11:06 +0800

    简单的批处理，不能跳过错误的。

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -7,6 +7,57 @@ use user::{exec, fork, println, wait, yield_};
 
 #[no_mangle]
 fn main() -> i32 {
+    let tests = [
+        "brk\0",
+        "clone\0",
+        "dup\0",
+        "execve\0",
+        "fork\0",
+        "getcwd\0",
+        "getpid\0",
+        "gettimeofday\0",
+        "mmap\0",
+        // "mount\0",
+        "open\0",
+        // "pipe\0",
+        "times\0",  //好像有点问题？
+        "uname\0",
+        "wait\0",
+        "write\0",
+        "chdir\0",
+        "close\0",
+        "dup2\0",
+        "exit\0",
+        "fstat\0",
+        "getdents\0",
+        "getppid\0",
+        "mkdir_\0",
+        "munmap\0",
+        "openat\0",
+        "read\0",
+        "sleep\0",
+        // "umount\0",
+        // "unlink\0",
+        "waitpid\0",
+        "yield\0",
+    ];
+
+    for prog in tests {
+        let pid = fork();
+        if pid == 0 {
+            println!("Running {}", prog);
+            exec(prog, &[core::ptr::null()]);
+            panic!("exec failed");
+        } else {
+            let mut exit_code: i32 = 0;
+            wait(&mut exit_code);
+            if pid == -1 {
+                yield_();
+                continue;
+            }
+        }
+    }
+
     if fork() == 0 {
         println!("Exiting main...");
         exec("user_shell\0", &[core::ptr::null::<u8>()]);
